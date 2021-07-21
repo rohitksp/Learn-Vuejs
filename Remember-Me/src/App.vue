f4a72cee2a6cdc51bd54aa22abef75a84d84438e
Rohit Gupta <rohitgupta@Izingas-MacBook-Air.local>
2021-07-21 22:01:52 +0530

    I added one project that is 'Remember-Me'

---
+++ Remember-Me/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <the-header title="Remember Me"></the-header>
+  <stored-resources :resource="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/resources/StoredResources.vue'
+import TheHeader from './components/layouts/TheHeader.vue'
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official_guide',
+          title: 'Official Guide',
+          description: 'This is a office documents to vue.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Remember-Me/src/components/resources/StoredResources.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <learning-resource v-for="res in resource" :key="res.id" :res="res">
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resource'],
+  components: {
+    LearningResource
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
